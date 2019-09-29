2f7eb76012384c428165167078abab0a9a523b2d
Camilo <c.acevedo1993@gmail.com>
2019-09-28 21:14:00 -0500

    Initial commit

---
+++ components/footer-inside.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="footer d-flex">
+    <div class="col-12 m-auto pt-5">
+      <div class="row">
+        <ul class="categorie-list p-2 m-auto">
+          <li class="footer-links">RECENT UPDATES</li>
+          <li class="footer-links">ABOUT</li>
+          <li class="footer-links">PRIVACY POLICY</li>
+          <li class="footer-links">SPONSORSHIP</li>
+        </ul>
+      </div>
+      <div class="row">
+        <ul class="categorie-list p-2 m-auto">
+          <li>Developed by Camilo Acevedo Escalante</li>
+        </ul>
+      </div>
+      <div class="row">
+        <ul class="categorie-list p-4 m-auto">
+          <li>
+            <i class="fab fa-twitter"></i>
+          </li>
+          <li>
+            <i class="fab fa-github"></i>
+          </li>
+          <li>
+            <i class="fab fa-hackerrank"></i>
+          </li>
+          <li>
+            <i class="fas fa-globe"></i>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped>
+.footer {
+  background: #293347;
+}
+
+.categorie-list li {
+  display: inline;
+  margin: 1.3em;
+  font-weight: 800;
+  color: #fff;
+  padding: 1em;
+}
+
+.footer-links:hover {
+  background: #ff5c03;
+  border-radius: 0.25rem;
+  transition: 0.5s;
+}
+
+.categorie-list i {
+  font-size: 2em;
+}
+
+.categorie-list i:hover {
+  color: #ff5c03;
+  transition: 0.5s;
+}
+</style>
---
+++ components/header-navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="navbar navbar-expand-lg navbar-light navbar-header p-3">
+    <a class="navbar-brand brand" href="#">
+      <b>[ I N S I D E ]</b>
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- collapse -->
+    <div class="navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ml-auto">
+        <a class="nav-item nav-link ml-5" href="#">
+          <i class="fas fa-search navbar-icon"></i>
+        </a>
+        <a class="nav-item nav-link ml-5" href="#">
+          <i class="far fa-user navbar-icon"></i>
+        </a>
+        <a class="btn-sponsorship ml-5">SPONSORSHIP</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped>
+.navbar-header {
+  background: #fff !important;
+}
+
+.brand {
+  font-size: 2em;
+  font-weight: 800;
+  color: #ff5c03;
+}
+
+.navbar-icon {
+  font-size: 1.8em;
+  color: #ff5c03;
+}
+
+.btn-sponsorship {
+  color: #ff5c03 !important;
+  padding: 0.6em;
+  border: 1px solid;
+  border-color: #ff5c03;
+  border-radius: 0.25rem;
+}
+
+.btn-sponsorship:hover {
+  color: #fff !important;
+  background: #ff5c03;
+  transition: 0.5s;
+}
+</style>
